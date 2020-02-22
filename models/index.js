c4ccff94d638e0705e74e5c006251754309fc36d
Yue Zheng <zheng.yue1@husky.neu.edu>
2020-02-22 02:41:05 -0500

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,8 +1,6 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(
-  "postgres://dczzpyzf:cCC9QylT9kO6Afmgcg0Ie_xEnEedM4WE@isilo.db.elephantsql.com:5432/dczzpyzf"
-);
+const sequelize = new Sequelize("postgres URL");
 
 const db = {
   User: sequelize.import("./user")
